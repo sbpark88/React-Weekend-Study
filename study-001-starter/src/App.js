8340e002f8c44641bf4b68a8e2532f7215f124fc
sbpark88 <saebyulpark88@gmail.com>
2023-06-24 11:58:59 +0900

    Add: Header, Footer, Section Component

--- study-001-starter/src/App.js
+++ study-001-starter/src/App.js
@@ -1,9 +1,12 @@
-function App() {
-  return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-  );
-}
+import {Header, Footer, Section} from "./Common";
+
+const App = () => (
+    <>
+      <Header/>
+      <Section/>
+      <Footer/>
+    </>
+)
+
 
 export default App;
--- study-001-starter/src/index.js
+++ study-001-starter/src/index.js
@@ -1,13 +1,8 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>,
+    <App/>,
     document.getElementById('root')
 );
