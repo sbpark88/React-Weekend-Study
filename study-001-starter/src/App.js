6061aceab33df006daa4f1e08610b21a74fb7581
sbpark88 <saebyulpark88@gmail.com>
2023-07-01 11:18:06 +0900

    Feat: Import Image in React

--- study-001-starter/src/App.js
+++ study-001-starter/src/App.js
@@ -1,7 +1,14 @@
 import "./css/style.scss";
+import ImportImage from "./Component/ImageImport/ImportImage";
+import WebFont from "./Component/WebFont/WebFont";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      {/*<ImportImage />*/}
+      <WebFont />
+    </>
+  );
 };
 
 export default App;
