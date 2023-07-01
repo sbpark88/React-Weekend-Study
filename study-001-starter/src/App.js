082365833208cdd2a7d9c444d6a7666e95aa3a1a
sbpark88 <saebyulpark88@gmail.com>
2023-07-01 10:09:00 +0900

    Update: useState control
    - Change main text color
    - Send parent's setter to children

--- study-001-starter/src/App.js
+++ study-001-starter/src/App.js
@@ -3,14 +3,30 @@ import Popup from "./Popup";
 import { useState } from "react";
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
+  const [style, setStyle] = useState({
+    color: "blue",
+  });
 
   return (
     <>
       <main>
-        <h1 onClick={() => setVisible((old) => !old)}>메인 콘텐츠</h1>
-        {visible && <Popup />}
+        <h1 style={style}>메인 콘텐츠</h1>
+        <sectoin>
+          메인 콘텐츠 색상 변경 :
+          <button onClick={(e) => setStyle({ color: e.target.innerText })}>
+            blue
+          </button>
+          <button onClick={(e) => setStyle({ color: e.target.innerText })}>
+            red
+          </button>
+        </sectoin>
+        <section>
+          Popup 관리 :
+          <button onClick={() => setVisible(true)}>Open Popup</button>
+        </section>
       </main>
+      {visible && <Popup setVisible={setVisible} />}
     </>
   );
 };
--- study-001-starter/src/Popup.js
+++ study-001-starter/src/Popup.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Popup extends Component {
-  const;
-  popStyle = {
+function Popup({ setVisible }) {
+  const popStyle = {
     width: 600,
     height: 300,
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "end",
+    alignItems: "start",
     backgroundColor: "#555",
     fontSize: 30,
     color: "#fff",
@@ -17,9 +16,18 @@ class Popup extends Component {
     transform: "translate(-50%, -50%)",
   };
 
-  render() {
-    return <aside style={this.popStyle}>Popup</aside>;
-  }
+  const closePopupStyle = {
+    position: "absolute",
+    top: "20px",
+    right: "30px",
+  };
+  return (
+    <aside style={popStyle}>
+      <span onClick={() => setVisible(false)} style={closePopupStyle}>
+        Close Popup
+      </span>
+    </aside>
+  );
 }
 
 export default Popup;
