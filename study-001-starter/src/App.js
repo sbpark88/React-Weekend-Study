3d2665ae262a31432724fb88f07989f7ef911ddd
sbpark88 <saebyulpark88@gmail.com>
2023-07-01 10:09:37 +0900

    Chore: init

--- study-001-starter/src/App.js
+++ study-001-starter/src/App.js
@@ -1,34 +1,6 @@
 import "./css/style.scss";
-import Popup from "./Popup";
-import { useState } from "react";
-
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  const [style, setStyle] = useState({
-    color: "blue",
-  });
-
-  return (
-    <>
-      <main>
-        <h1 style={style}>메인 콘텐츠</h1>
-        <sectoin>
-          메인 콘텐츠 색상 변경 :
-          <button onClick={(e) => setStyle({ color: e.target.innerText })}>
-            blue
-          </button>
-          <button onClick={(e) => setStyle({ color: e.target.innerText })}>
-            red
-          </button>
-        </sectoin>
-        <section>
-          Popup 관리 :
-          <button onClick={() => setVisible(true)}>Open Popup</button>
-        </section>
-      </main>
-      {visible && <Popup setVisible={setVisible} />}
-    </>
-  );
+  return <></>;
 };
 
 export default App;
