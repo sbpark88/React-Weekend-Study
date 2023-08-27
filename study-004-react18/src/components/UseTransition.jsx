edd9d7d792c16d8e2b0ae25254285c213d5f56eb
sbpark88 <saebyulpark88@gmail.com>
2023-08-27 11:03:42 +0900

    Add: With useTransition
    - useTransition 을 사용하면
      setTimeout(() => {}, 0) 을
      사용하기 편하게 해준다.

--- study-004-react18/src/components/UseTransition.jsx
+++ study-004-react18/src/components/UseTransition.jsx
@@ -1,23 +1,21 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useTransition } from "react";
 
 function UseTransition(props) {
   const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
-  const [isPending, setIsPending] = useState(false);
+  const [isPending, startTransition] = useTransition();
   console.log(isPending);
 
   const onButtonClick = useCallback(
     (event) => {
       setCount((prevState) => prevState + 1);
-      setIsPending(true);
-      setTimeout(() => {
+      startTransition(() => {
         const largeArray = Array.from(
           { length: 30000 },
           (_, index) => count + index,
         );
         setArray(largeArray);
-        setIsPending(false);
-      }, 0);
+      });
     },
     [count],
   );
@@ -41,4 +39,5 @@ export default UseTransition;
 /*
  * Use Transition
  * : 특정 handler 함수에 무거운 연산이 있을 경우, 해당 연산을 분리시켜 rendering 지연을 해소함.
+ * : setTimeout(() => {}, 0) 을 사용하기 편하게 해준다.
  * */
