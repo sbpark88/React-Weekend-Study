16ac81c05bbc3764e010cfb7209bf9d44e0467ca
sbpark88 <saebyulpark88@gmail.com>
2023-09-02 09:09:51 +0900

    Update: Sass config

--- study-005-nextjs/next.config.js
+++ study-005-nextjs/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePath: [path.join(__dirname, "style")],
+    prependData: `@import 'styles/_variables.scss';`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
