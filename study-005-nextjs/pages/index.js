d04f746e1c71b96a831bf7f17824493758359a5d
sbpark88 <saebyulpark88@gmail.com>
2023-09-02 09:16:02 +0900

    Style: Apply module sass

--- study-005-nextjs/pages/index.js
+++ study-005-nextjs/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.scss";
+import styles from "@/pages/Home.module.scss";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}>
+      <main className={styles.main}>
         <h1>Hello</h1>
       </main>
     </>
