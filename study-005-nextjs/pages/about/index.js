2e6a2d1bb63ad9c256f31935af97a9e4e4e6fe6c
sbpark88 <saebyulpark88@gmail.com>
2023-09-06 01:29:15 +0900

    Update: Apply React icons

--- study-005-nextjs/pages/about/index.js
+++ study-005-nextjs/pages/about/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./About.module.scss";
+import { IconContext } from "react-icons";
+import { FaAppStore } from "react-icons/fa6";
+import { FcApproval, FcCamera } from "react-icons/fc";
+import { HiChat } from "react-icons/hi";
 
 function About(props) {
   return (
@@ -8,7 +12,21 @@ function About(props) {
         <h1>About</h1>
         <div>This is not "Dongle" font. </div>
         <p>This is "Dongle" font.</p>
+        <br />
       </main>
+      <section>
+        <FaAppStore size="100" color="blue" />
+        <IconContext.Provider
+          value={{
+            size: 50,
+            className: "my-icons",
+          }}
+        >
+          <FcApproval />
+          <FcCamera />
+          <HiChat />
+        </IconContext.Provider>
+      </section>
     </>
   );
 }
