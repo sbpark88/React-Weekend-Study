7abd86661ace327fe6dffc4e8f3021ee0b48281b
sbpark88 <saebyulpark88@gmail.com>
2023-09-06 15:40:13 +0900

    Update: Next.js default mock server
    MSW 없이도 Next.js 는 Node.js 를
    이용한 Mock 서버를 내장하고있다.

--- study-005-nextjs/pages/post/index.js
+++ study-005-nextjs/pages/post/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Post(props) {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleClick = () => {
+    fetch("/api/hello", {
+      method: "Post",
+      body: "abc",
+      // next 가 application/json 은 기본으로 설정해줌
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <>
       <main>
         <p>게시글 목록 페이지</p>
+        <button onClick={handleClick}>서버 데이터 변경</button>
       </main>
     </>
   );
